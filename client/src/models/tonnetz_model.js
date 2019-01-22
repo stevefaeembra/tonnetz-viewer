bd3c908fe4c59899d5ff1a84b981ecfae221f1a5
Steven Kay <stevendkay@gmail.com>
2019-01-22 21:47:03 +0000

    inverted Y axis, now how i remember it...

--- client/src/models/tonnetz_model.js
+++ client/src/models/tonnetz_model.js
@@ -37,6 +37,7 @@ TonnetzModel.prototype.getScaleNotes = function (tonicKey, scaleName) {
 TonnetzModel.prototype.getNoteAt = function (row,col) {
   // return key (0-11 for C through B in semitone increments)
   // for position row, col (0,0 is top left and starts at C)
+  row = 6-row;
   let horizontal = (0+(7*col))%12;
   return Math.abs((horizontal+(+4*row))%12);
 };
