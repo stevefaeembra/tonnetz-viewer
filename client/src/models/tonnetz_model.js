a4d91a3a960088bce39ddebb55bda879b8574572
Steven Kay <stevendkay@gmail.com>
2019-01-22 21:30:59 +0000

    added lots of juicy scales

--- client/src/models/tonnetz_model.js
+++ client/src/models/tonnetz_model.js
@@ -3,8 +3,21 @@ const PubSub = require("../helpers/pub_sub");
 const TonnetzModel = function() {
   this.notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
   this.scales = {
-    "major": [0,2,4,5,7,9,11],
-    "minor": [0,2,3,5,7,9,11]
+    "major":[0,2,4,5,7,9,11],
+    "minor":[0,2,3,5,7,8,10],
+    "dorian":[0,2,3,5,7,9,10],
+    "phygrian":[0,1,3,5,7,8,10],
+    "lydian":[0,2,4,6,7,9,11],
+    "mixolydian":[0,2,4,5,7,9,10],
+    "locrian":[0,1,3,5,6,8,10],
+    "minor-pentatonic":[0,3,5,7,10],
+    "major-pentatonic":[0,2,4,7,9],
+    "whole-tone":[0,2,4,6,8,10],
+    "acoustic":[0,2,4,6,7,9,10],
+    "octatonic-1":[0,2,3,5,6,8,9,11],
+    "octatonic-2":[0,1,3,4,6,7,9,10],
+    "ionian":[0,2,4,5,7,9,11], // same as major
+    "aeolian":[0,2,3,5,7,8,10], // same as minor
   }
 }
 
