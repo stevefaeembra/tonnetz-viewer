1f253af7a52db59ba22a1e424f5760ed158898a4
Steven Kay <stevendkay@gmail.com>
2019-01-24 19:36:16 +0000

    clicking scale publishes change message

--- client/src/views/key_scale_button_view.js
+++ client/src/views/key_scale_button_view.js
@@ -1,3 +1,5 @@
+const PubSub = require("../helpers/pub_sub");
+
 const KeyScaleButtonView = function(slot, scaleName) {
   this.slot = slot; // 1 or 2 for key1, key2
   this.scaleName = scaleName;
@@ -6,7 +8,16 @@ const KeyScaleButtonView = function(slot, scaleName) {
 KeyScaleButtonView.prototype.render = function () {
   let divButton = document.createElement("div");
   divButton.className = "keyscale-button";
+  divButton.id = `keyscale_${this.slot}_${this.scaleName}`;
   divButton.innerHTML = this.scaleName;
+  divButton.addEventListener("click",(event) => {
+    const target = event.currentTarget;
+    const id = target.id;
+    const tokens = id.split("_");
+    const slot = tokens[1];
+    const scalename = tokens[2];
+    PubSub.publish("scaleChange", {"slot":slot, "scalename": scalename});
+  });
   return divButton;
 };
 
