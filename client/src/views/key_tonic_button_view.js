e67fce8232b0edb093a835c944790176e15c14ed
Steven Kay <stevendkay@gmail.com>
2019-01-24 19:38:30 +0000

    now publish clicks on key tonic

--- client/src/views/key_tonic_button_view.js
+++ client/src/views/key_tonic_button_view.js
@@ -1,3 +1,5 @@
+const PubSub = require("../helpers/pub_sub");
+
 const KeyTonicButtonView = function(slot, noteNumber, noteName) {
   this.slot = slot; // 1 or 2 for key1, key2
   this.noteNumber = noteNumber;
@@ -8,6 +10,16 @@ KeyTonicButtonView.prototype.render = function () {
   let divButton = document.createElement("div");
   divButton.className = "keytonic-button";
   divButton.innerHTML = this.noteName;
+  divButton.id = `keytonic_${this.slot}_${this.noteName}`;
+  divButton.innerHTML = this.noteName;
+  divButton.addEventListener("click",(event) => {
+    const target = event.currentTarget;
+    const id = target.id;
+    const tokens = id.split("_");
+    const slot = tokens[1];
+    const notename = tokens[2];
+    PubSub.publish("tonicChange", {"slot":slot, "notename": notename});
+  });
   return divButton;
 };
 
